b63d3280003a3bd9baf2871c3b2f386f766acb23
patrickgeorge1 <patrionpatrick@gmail.com>
2020-02-03 14:49:23 -0800

    read string

--- CItiri si Scrieri/file.asm
+++ CItiri si Scrieri/file.asm
@@ -1,20 +1,29 @@
 %include "includes/io.inc"
-
-
 extern scanf
-section .data
+extern stdin
+extern gets
 
+
+section .data
     scanf_format dd "%d", 0
+    scanf_format_string dd "%s", 0
+
+
 section .bss 
-    arr times 100 resb 4
+    arr times 100 resd 1
     n resb 4
+    string times 200 resb 1
+
 
 section .text
+
+
 global CMAIN
 CMAIN:
     mov ebp, esp; for correct debugging
     
-    
+   	;call citire   ;string
+
     
     ; citeste n
     push n
@@ -28,17 +37,17 @@ CMAIN:
 
     ; citeste elementele din array
     mov ecx, 0
-read_array:
-    lea eax, [arr + 4 * ecx]
-    push ecx
-    push dword eax
-    push scanf_format
-    call scanf
-    add esp, 8
-    pop ecx
-    inc ecx
-    cmp ecx, [n]
-    jl read_array
+	read_array:
+	    lea eax, [arr + 4 * ecx]
+	    push ecx
+		    push dword eax
+		    push scanf_format
+		    call scanf
+		    add esp, 8
+	    pop ecx
+	    inc ecx
+	    cmp ecx, [n]
+	jl read_array
  
     
        
@@ -46,19 +55,35 @@ read_array:
     PRINT_STRING "Array: "
     mov ecx, 0
     lea eax, [arr]
-print_array:
-    PRINT_UDEC 4, [eax]
-    PRINT_CHAR ' '
-    inc ecx
-    add eax, 4
-    cmp ecx, [n]
-    jl print_array
-
-
-
+	print_array:
+	    PRINT_UDEC 4, [eax]
+	    PRINT_CHAR ' '
+	    inc ecx
+	    add eax, 4
+	    cmp ecx, [n]
+	jl print_array
 
 
 
     xor eax, eax
     mov ebp, esp
     ret
+
+
+citire:
+	enter 0, 0
+
+	sub esp, 12
+	    push esp
+	    call gets
+	    add esp, 4
+
+	NEWLINE
+	PRINT_CHAR [esp]
+	NEWLINE
+	PRINT_CHAR [esp+1]
+	NEWLINE
+	PRINT_CHAR [esp+2]
+
+	leave
+	ret
